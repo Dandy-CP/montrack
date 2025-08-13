1ca7d5e4fdd5c059d1333ed8e38f937763975604
Dandy Candra <dandycandrapratama@gmail.com>
2025-08-13 22:19:10 +0700

    feat: add new routes and screens
    This commit introduces several new routes and screens to the application.
    
    **Authentication and Security:**
    - `/otp`: OTP verification screen.
    - `/input-pin`: Screen for setting up, inputting, or disabling a PIN.
    - `/2fa`: Screen for setting up or disabling two-factor authentication.
    
    **Wallet Management:**
    - `/wallet`: Wallet overview screen.
    - `/wallet/create`: Screen for creating a new wallet.
    - `/wallet/edit`: Screen for editing an existing wallet.
    
    **Pocket Management:**
    - `/pocket/detail`: Screen for viewing pocket details.
    - `/pocket/create`: Screen for creating a new pocket.
    - `/pocket/edit`: Screen for editing an existing pocket.
    
    **Goals Management:**
    - `/goals/create`: Screen for creating a new goal.
    - `/goals/edit`: Screen for editing an existing goal.
    
    **Transactions:**
    - `/create-transaction`: Screen for creating a new transaction.
    - `/spending`: Screen for viewing spending, filterable by transaction type.
    
    **Route Changes:**
    - Renamed `/income` to `/spending` and updated it to handle different transaction types.
    - Renamed `/expense` to `/spending` and updated it to handle different transaction types.
    - `/pocket/detail` now has the name `pocket-detail`.
    - `/pocket/create` now has the name `create-pocket`.
    - `/goals/create` now has the name `create-goals`.

--- lib/config/router.dart
+++ lib/config/router.dart
@@ -1,17 +1,25 @@
 import 'package:go_router/go_router.dart';
 import 'package:montrack/root.dart';
-import 'package:montrack/screen/expense.dart';
+import 'package:montrack/screen/create_transaction.dart';
 import 'package:montrack/screen/goals/create_goals.dart';
+import 'package:montrack/screen/goals/edit_goals.dart';
 import 'package:montrack/screen/goals/goals.dart';
 import 'package:montrack/screen/goals/goals_detail.dart';
-import 'package:montrack/screen/income.dart';
+import 'package:montrack/screen/input_pin.dart';
 import 'package:montrack/screen/login.dart';
 import 'package:montrack/screen/onboarding.dart';
+import 'package:montrack/screen/otp.dart';
 import 'package:montrack/screen/pocket/create_pocket.dart';
+import 'package:montrack/screen/pocket/edit_pocket.dart';
 import 'package:montrack/screen/pocket/pocket.dart';
 import 'package:montrack/screen/pocket/pocket_detail.dart';
+import 'package:montrack/screen/profile/screen_2fa.dart';
 import 'package:montrack/screen/signup.dart';
+import 'package:montrack/screen/spending.dart';
 import 'package:montrack/screen/tab_screen.dart';
+import 'package:montrack/screen/wallet/create_wallet.dart';
+import 'package:montrack/screen/wallet/edit_wallet.dart';
+import 'package:montrack/screen/wallet/wallet.dart';
 
 final GoRouter router = GoRouter(
   initialLocation: '/',
@@ -38,18 +46,96 @@ final GoRouter router = GoRouter(
       builder: (context, state) => Signup(),
     ),
     GoRoute(
+      path: '/otp',
+      name: 'otp',
+      builder: (context, state) {
+        final userId = state.uri.queryParameters['userId'] ?? '';
+
+        return OTPScreen(userId: userId);
+      },
+    ),
+    GoRoute(
+      path: '/input-pin',
+      name: 'input-pin',
+      builder: (context, state) {
+        final title = state.uri.queryParameters['title'] ?? '';
+        final type = state.uri.queryParameters['type'] ?? '';
+        final selectedType = type == 'setup'
+            ? PinType.setup
+            : type == 'disabled'
+            ? PinType.disabled
+            : PinType.input;
+
+        return InputPinScreen(title: title, type: selectedType);
+      },
+    ),
+    GoRoute(
+      path: '/2fa',
+      name: '2fa',
+      builder: (context, state) {
+        final type = state.uri.queryParameters['type'] ?? '';
+        final selectedType = type == 'setup' ? Type2FA.setup : Type2FA.disabled;
+
+        return Screen2FA(type: selectedType);
+      },
+    ),
+
+    // Wallet Route
+    GoRoute(
+      path: '/wallet',
+      name: 'wallet',
+      builder: (context, state) => WalletScreen(),
+    ),
+    GoRoute(
+      path: '/wallet/create',
+      builder: (context, state) => CreateWalletScreen(),
+    ),
+    GoRoute(
+      path: '/wallet/edit',
+      builder: (context, state) {
+        final walletId = state.uri.queryParameters['walletId'] ?? '';
+        final walletName = state.uri.queryParameters['walletName'] ?? '';
+        final walletAmount = state.uri.queryParameters['walletAmount'] ?? '';
+
+        return EditWalletScreen(
+          walletId: walletId,
+          walletName: walletName,
+          walletAmount: walletAmount,
+        );
+      },
+    ),
+
+    // Pocket Route
+    GoRoute(
       path: '/pocket',
       name: 'pocket',
       builder: (context, state) => PocketScreen(),
     ),
     GoRoute(
       path: '/pocket/detail',
+      name: 'pocket-detail',
       builder: (context, state) {
         final pocketId = state.uri.queryParameters['pocketId'] ?? '';
 
         return PocketDetailScreen(pocketId: pocketId);
       },
     ),
+    GoRoute(
+      path: '/pocket/create',
+      name: 'create-pocket',
+      builder: (context, state) => CreatePocket(),
+    ),
+    GoRoute(
+      path: '/pocket/edit',
+      name: 'edit-pocket',
+      builder: (context, state) {
+        final pocketId = state.uri.queryParameters['pocketId'] ?? '';
+
+        return EditPocket(pocketId: pocketId);
+      },
+    ),
+
+    // Goals Route
     GoRoute(
       path: '/goals',
       name: 'goals',
@@ -64,24 +150,31 @@ final GoRouter router = GoRouter(
       },
     ),
     GoRoute(
-      path: '/income',
-      name: 'income',
-      builder: (context, state) => IncomeScreen(),
-    ),
-    GoRoute(
-      path: '/expense',
-      name: 'expense',
-      builder: (context, state) => ExpenseScreen(),
-    ),
-    GoRoute(
-      path: '/create-pocket',
-      name: 'create-pocket',
-      builder: (context, state) => CreatePocket(),
-    ),
-    GoRoute(
-      path: '/create-goals',
+      path: '/goals/create',
       name: 'create-goals',
       builder: (context, state) => CreateGoals(),
+    ),
+    GoRoute(
+      path: '/goals/edit',
+      builder: (context, state) {
+        final pocketId = state.uri.queryParameters['goalsId'] ?? '';
+
+        return EditGoalsScreen(goalsId: pocketId);
+      },
+    ),
+
+    GoRoute(
+      path: '/create-transaction',
+      name: 'create-transaction',
+      builder: (context, state) => CreateTransaction(),
+    ),
+    GoRoute(
+      path: '/spending',
+      builder: (context, state) {
+        final type = state.uri.queryParameters['transactionType'] ?? '';
+
+        return SpendingScreen(transactionType: type);
+      },
     ),
   ],
 );
