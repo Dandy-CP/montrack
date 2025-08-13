49d9cd9502ec9a3fb4bd395479ac62fb683f42d9
Dandy Candra <dandycandrapratama@gmail.com>
2025-08-13 22:20:14 +0700

    Refactor: Update date formatting
    This commit updates the `formattedDate` function in `lib/utils/formated_date.dart` to change the date format from 'dd/MM/yyyy, hh:mm' to 'dd/MM/yyyy'.

--- lib/utils/formated_date.dart
+++ lib/utils/formated_date.dart
@@ -1,7 +1,5 @@
 import 'package:intl/intl.dart';
 
 String formattedDate(String dateStringValue) {
-  return DateFormat(
-    'dd/MM/yyyy, hh:mm',
-  ).format(DateTime.parse(dateStringValue));
+  return DateFormat('dd/MM/yyyy').format(DateTime.parse(dateStringValue));
 }
